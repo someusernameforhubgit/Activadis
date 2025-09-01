f9265102f1959fef9990620294e36c35e900f09f
username <abc@xyz>
2025-09-01 15:12:38 +0200

    Init

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('gebruiker'));
+app.use("/beheerder", express.static("beheerder"));
+
+app.listen(3000, () => {
+    console.log('Server listening on http://localhost:3000');
+});
