33e65988f5e20f155ae2cde07f7503d6d387e63e
RowanWays <rowanways@icloud.com>
2025-09-20 20:28:40 +0200

    Hidden Database Function

--- database/api/activiteit.js
+++ database/api/activiteit.js
@@ -5,17 +5,17 @@ const url = "/api/activiteit";
 export default function ActiviteitAPI(app) {
     app.get(url, async (req, res) => {
         if (req.query.id) {
-            const activiteit = (await database.query("SELECT * FROM activiteit WHERE id = ?", [req.query.id]))[0];
+            const activiteit = (await database.query("SELECT * FROM activiteit WHERE id = ? AND hidden = 0", [req.query.id]))[0];
             res.send(activiteit);
         } else {
-            const activiteiten = await database.query("SELECT * FROM activiteit");
+            const activiteiten = await database.query("SELECT * FROM activiteit WHERE hidden = 0");
             res.send(activiteiten);
         }
     });
 
     app.post(url, async (req, res) => {
-        if (req.body.naam && req.body.locatie && req.body.eten && req.body.omschrijving && req.body.begin && req.body.eind && req.body.kost && req.body.max && req.body.min && req.body.afbeelding) {
-            const activiteit = await database.query("INSERT INTO activiteit (naam, locatie, eten, omschrijving, begin, eind, kost, max, min, afbeelding) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [req.body.naam, req.body.locatie, req.body.eten, req.body.omschrijving, req.body.begin, req.body.eind, req.body.kost, req.body.max, req.body.min, req.body.afbeelding]);
+        if (req.body.naam && req.body.locatie && req.body.eten && req.body.omschrijving && req.body.begin && req.body.eind && req.body.kost && req.body.max && req.body.min && req.body.afbeelding && req.body.hidden !== undefined) {
+            const activiteit = await database.query("INSERT INTO activiteit (naam, locatie, eten, omschrijving, begin, eind, kost, max, min, afbeelding, hidden) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", [req.body.naam, req.body.locatie, req.body.eten, req.body.omschrijving, req.body.begin, req.body.eind, req.body.kost, req.body.max, req.body.min, req.body.afbeelding, req.body.hidden ? 1 : 0]);
             res.send(activiteit);
         } else {
             res.status(400).send("One or more required fields are missing");
@@ -23,8 +23,8 @@ export default function ActiviteitAPI(app) {
     });
 
     app.put(url, async (req, res) => {
-        if (req.body.id && req.body.naam && req.body.locatie && req.body.eten && req.body.omschrijving && req.body.begin && req.body.eind && req.body.kost && req.body.max && req.body.min && req.body.afbeelding) {
-            const activiteit = await database.query("UPDATE activiteit SET naam = ?, locatie = ?, eten = ?, omschrijving = ?, begin = ?, eind = ?, kost = ?, max = ?, min = ?, afbeelding = ? WHERE id = ?", [req.body.naam, req.body.locatie, req.body.eten, req.body.omschrijving, req.body.begin, req.body.eind, req.body.kost, req.body.max, req.body.min, req.body.afbeelding, req.body.id]);
+        if (req.body.id && req.body.naam && req.body.locatie && req.body.eten && req.body.omschrijving && req.body.begin && req.body.eind && req.body.kost && req.body.max && req.body.min && req.body.afbeelding && req.body.hidden !== undefined) {
+            const activiteit = await database.query("UPDATE activiteit SET naam = ?, locatie = ?, eten = ?, omschrijving = ?, begin = ?, eind = ?, kost = ?, max = ?, min = ?, afbeelding = ?, hidden = ? WHERE id = ?", [req.body.naam, req.body.locatie, req.body.eten, req.body.omschrijving, req.body.begin, req.body.eind, req.body.kost, req.body.max, req.body.min, req.body.afbeelding, req.body.hidden ? 1 : 0, req.body.id]);
             res.send(activiteit);
         } else {
             res.status(400).send("One or more required fields are missing");
